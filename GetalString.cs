eef4907d00f92b33da3afa369b1f32770c0da1a3
Karim Altir <mhd.altir@student.ehb.be>
2023-09-25 17:44:30 +0200

    changing the 'value' into 'Getal' + deleting the unnecessary using systems

--- GetalString.cs
+++ GetalString.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace Eigenshappen
 {
@@ -13,7 +8,7 @@ namespace Eigenshappen
 
         public GetalString(string Getal)
         {
-            string getalProper = Regex.Replace(value, "[^0-9-]", "");
+            string getalProper = Regex.Replace(Getal, "[^0-9-]", "");
 
             if (string.IsNullOrEmpty(getalProper) || getalProper == "-")
             {
