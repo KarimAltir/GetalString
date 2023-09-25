86ac419d61f5bc0ca47504e8d7d50c72f720870d
Karim Altir <mhd.altir@student.ehb.be>
2023-09-25 17:56:59 +0200

    creating an Instance + make the toString more clearly

--- Program.cs
+++ Program.cs
@@ -7,5 +7,10 @@ List<GetalString> getalString = new List<GetalString>();
 Console.Write("Geef de tekst in:");
 getal = Console.ReadLine();
 
-foreach (GetalString getalStrings in getalString)
-    Console.WriteLine(getalStrings.ToString());
+GetalString getalInstance = new GetalString(getal);
+getalString.Add(getalInstance);
+
+foreach (GetalString getalStringInstance in getalString)
+{
+    Console.WriteLine(getalStringInstance.ToString());
+}
